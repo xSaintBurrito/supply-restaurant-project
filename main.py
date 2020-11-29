2575e93e6526501a40b46499edc6a141eb328f9a
zofia.durys <zofia.durys@stazysta.comarch.pl>
2020-11-29 21:27:02 +0100

    Second draft of the restaurant. The table is wonky.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import json
+
+from dash_table import DataTable
 import flask
 import pandas as pd
 import dash_bootstrap_components as dbc
@@ -6,7 +8,6 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 import plotly.express as px
-
 
 from app import app, server
 
@@ -23,10 +24,14 @@ def check_pantry():
     pantry = pd.read_csv("Data\\Pantry.csv", sep=",")
     pantry.columns = ["Groceries", "Quantity"]
 
-    #result = pantry.to_json(orient="records")
-    #parsed_pantry = json.loads(result)
-
     return pantry
+
+
+def check_orders():
+    orders = pd.read_csv("Data\\Orders.csv", sep=",")
+    orders.columns = ["Order Nr.", "Item", "Quantity"]
+
+    return orders
 
 
 def json_orders():
@@ -57,50 +62,53 @@ navbar = dbc.NavbarSimple(
     children=[
         dbc.NavItem(html.Img(src=app.get_asset_url("logo.png"), height="50px", width="auto", className="ml-2")),
     ],
-    brand="Internet Applications Programming",
+    brand="Simple Restaurant",
     color="info",
     dark=True
 )
 
-header = dbc.Card(
-    dbc.Row(
-        [
-            html.H1("Simple Restaurant", className="display-3")
-        ],
-        justify="center",
-    )
-)
-
-
 pantry = dbc.Card(
     dbc.Row(
         [
-            html.H1("Dynamically rendered tab content"),
-            dcc.Graph(id='example-graph', figure=render_graph())
+            dbc.Col(
+                [
+                    html.H1("Current Orders", style={'textAlign': 'center'}),
+                    DataTable(
+                        data=check_orders().to_dict('records'),
+                        columns=[{'id': c, 'name': c} for c in check_orders().columns],
+                        style_table={'width': '70%', 'overflowX': 'scroll'},
+                        style_header={'backgroundColor': 'rgb(230, 230, 230)', 'fontWeight': 'bold'},
+                        style_cell={'textAlign': 'center'},
+                        style_cell_conditional=
+                        [
+                            {'if': {'column_id': 'Order Nr.'}, 'width': '20%'},
+                            {'if': {'column_id': 'Quantity'}, 'width': '20%'}
+                        ]
+                    )
+                ], width=4),
+            dbc.Col(
+                [
+                    html.H1("Pantry Analysis", style={'textAlign': 'center'}),
+                    dcc.Graph(id='example-graph', figure=render_graph())
+                ], width=8),
         ],
-        justify="center",
+        justify="between",
     )
 )
 
 orders = dbc.Card(
-    dbc.Row(
-        [
-            html.H1("Orders"),
-        ],
-        justify="center",
-    )
+    html.H1("Orders", style={'textAlign': 'center'}),
 )
 
 app.layout = html.Div([
     navbar,
-    dbc.Container([
-        html.Div(header, id='header_div'),
-        html.Div(pantry, id='pantry_div'),
-        html.Div(orders, id='orders_div'),
-            ]
-        )
-    ])
-
+    dbc.Container(
+        [
+            html.Div(pantry, id='pantry_div'),
+            html.Div(orders, id='orders_div'),
+        ]
+    )
+])
 
 if __name__ == "__main__":
     app.run_server()
